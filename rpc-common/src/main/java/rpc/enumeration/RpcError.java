c37617860acc414101bc0c8a785531466060a9ce
coderLin <1543099786@qq.com>
2025-09-01 15:57:53 +0800

    feat: 实现编码器、解码器

--- rpc-common/src/main/java/rpc/enumeration/RpcError.java
+++ rpc-common/src/main/java/rpc/enumeration/RpcError.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum RpcError {
     SERVICE_INVOCATION_FAILURE("服务调用失败"),
     SERVICE_NOT_FOUND("找不到对应服务"),
-    SERVICE_NOT_IMPLEMENT_ANY_INTERFACE("注册的服务未实现接口");
+    SERVICE_NOT_IMPLEMENT_ANY_INTERFACE("注册的服务未实现接口"),
+    UNKNOWN_MAGIC_NUMBER("不识别的魔数"),
+    UNKNOWN_PACKAGE_TYPE("不识别的数据包类型"),
+    UNKNOWN_SERIALIZER("不识别的(反)序列化器");
 
     private final String message;
 }
