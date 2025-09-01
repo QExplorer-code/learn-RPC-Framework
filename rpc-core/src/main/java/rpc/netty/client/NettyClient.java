f7b2d099628beb078400d4e76e029df531799b9f
coderLin <1543099786@qq.com>
2025-09-01 15:11:19 +0800

    feat: netty服务端、客户端框架

---
+++ rpc-core/src/main/java/rpc/netty/client/NettyClient.java
@@ -0,0 +1,71 @@
+package rpc.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+import org.omg.PortableInterceptor.HOLDING;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rpc.common.RpcClient;
+import rpc.entity.RpcRequest;
+import rpc.entity.RpcResponse;
+
+public class NettyClient implements RpcClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyClient.class);
+
+    private String host;
+    private int port;
+
+    private static final Bootstrap bootstrap;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    static {
+        EventLoopGroup group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+//                        添加处理器
+//                        pipeline.addLast();
+                    }
+                });
+    }
+
+    @Override
+    public Object sendRequest(RpcRequest rpcRequest) {
+        try {
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            logger.info("连接到服务器{}：{}", host, port);
+            Channel channel = future.channel();
+            if (channel != null) {
+                channel.writeAndFlush(rpcRequest).addListener(future1 -> {
+                    if (future1.isSuccess()) {
+                        logger.info(String.format("客户端发送消息: %s", rpcRequest.toString()));
+                    } else {
+                        logger.error("发送消息时发生错误: ", future1.cause());
+                    }
+                });
+                channel.closeFuture().sync();
+                AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse");
+                RpcResponse rpcResponse = channel.attr(key).get();
+                return rpcResponse.getData();
+            }
+        } catch (InterruptedException e) {
+            logger.error("发送消息时发生错误: ", e);
+        }
+        return null;
+    }
+}
