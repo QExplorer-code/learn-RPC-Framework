d8da1e0738d57a507b4d809560e25ea2c0fa7662
coderLin <1543099786@qq.com>
2025-09-01 17:22:21 +0800

    feat: 完善netty服务端、客户端测试程序

--- test-client/src/main/java/TestNettyClient.java
+++ test-client/src/main/java/TestNettyClient.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import rpc.api.HelloObject;
+import rpc.api.HelloService;
+import rpc.common.RpcClient;
+import rpc.common.RpcClientProxy;
+import rpc.netty.client.NettyClient;
 
 public class TestNettyClient {
+    public static void main(String[] args) {
+        // 代理（连接服务端）
+        RpcClient nettyClient = new NettyClient("127.0.0.1", 9998);
+        RpcClientProxy rpcClientProxy = new RpcClientProxy(nettyClient);
+
+        // 服务调用
+        HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
+        HelloObject object = new HelloObject(12, "This is a message");
+        String res = helloService.hello(object);
+        System.out.println(res);
+    }
 }
--- test-server/src/main/java/test/TestNettyServer.java
+++ test-server/src/main/java/test/TestNettyServer.java
@@ -1,4 +1,20 @@
 package test;
 
+import rpc.api.HelloService;
+import rpc.netty.server.NettyServer;
+import rpc.registry.DefaultServiceRegistry;
+import rpc.registry.ServiceRegistry;
+
 public class TestNettyServer {
+    public static void main(String[] args) {
+        ServiceRegistry registry = new DefaultServiceRegistry();
+
+        // 服务注册
+        HelloService helloService = new HelloServiceImpl();
+        registry.register(helloService);
+
+        // 服务启动
+        NettyServer server = new NettyServer();
+        server.start(9998);
+    }
 }
