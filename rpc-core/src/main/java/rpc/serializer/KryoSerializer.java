ee3aebb40b23b35efd8c36bd5e9305b730276724
coderLin <1543099786@qq.com>
2025-09-02 10:25:00 +0800

    feat: 实现kryo序列化第一版

---
+++ rpc-core/src/main/java/rpc/serializer/KryoSerializer.java
@@ -0,0 +1,45 @@
+package rpc.serializer;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import rpc.enumeration.SerializerCode;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class KryoSerializer implements  CommonSerializer{
+
+    private static final ThreadLocal<Kryo> kryoLocal = new ThreadLocal<Kryo>() {
+        @Override
+        protected Kryo initialValue() {
+            Kryo kryo = new Kryo();
+            kryo.setReferences(true);
+            kryo.setRegistrationRequired(false);
+            return kryo;
+        }
+    };
+    @Override
+    public byte[] serialize(Object obj) {
+        Kryo kryo = kryoLocal.get();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        kryo.writeObject(output, obj);
+        output.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public Object deserialize(byte[] bytes, Class<?> clazz) {
+        Kryo kryo = kryoLocal.get();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Input input = new Input(byteArrayInputStream);
+        Object object = kryo.readObject(input, clazz);
+        return object;
+    }
+
+    @Override
+    public int getCode() {
+        return SerializerCode.KRYO.getCode();
+    }
+}
